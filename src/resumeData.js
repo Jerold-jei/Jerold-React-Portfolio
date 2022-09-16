1ee7fee34fd2161cc518925b7fdaf5af97af63ef
gpraj-cloud <gpraj@outlook.in>
2022-09-16 17:34:46 +0530

    new

---
+++ src/resumeData.js
@@ -0,0 +1,152 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Jerold Jayakumar",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/jerold-jayakumar-9a34081bb",
+    "skypeid": "jeroldjeis06@gmail.com",
+    "roleDescription": " I like to work in various parts of frontend, backend development and like to learn about new technologies, simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jerold-jayakumar-9a34081bb",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Jerold-jei",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"facebook",
+          "url":"https://www.facebook.com/jerold.jeyamups",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":"I am currently working on PHP Laravel, MySQL, HTML, and CSS. I have done an internship on Node JS and MongoDB. I am using MVC Model for software development and I am working in Agile. I am contributing to the live project.",
+    "address":"Tharamani, Chennai.",
+    "website":"https://github.com/Jerold-jei",
+    "education":[
+      {
+        "UniversityName":"St. Joseph's College, Trichy",
+        "specialization":"MCA Computer Application",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"81%"
+      },
+      {
+        "UniversityName":"Pondicherry University, Pondicherry",
+        "specialization":"MBA International Business",
+        "MonthOfPassing":"Sep",
+        "YearOfPassing":"2020",
+        "Achievements":"67%"
+      },
+      {
+        "UniversityName":"St. Joseph's College, Trichy",
+        "specialization":"B.Sc Physics",
+        "MonthOfPassing":"Apr",
+        "YearOfPassing":"2017",
+        "Achievements":"63%"
+      },
+      {
+        "UniversityName":"Amala Annai Hr Sec School, T. Sindalai Cherry",
+        "specialization":"XII",
+        "MonthOfPassing":"Apr",
+        "YearOfPassing":"2014",
+        "Achievements":"67%"
+      },
+      {
+        "UniversityName":"Amala Annai Hr Sec School, T. Sindalai Cherry",
+        "specialization":"X",
+        "MonthOfPassing":"Apr",
+        "YearOfPassing":"2012",
+        "Achievements":"79%"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Lead Integrated Business Services Pvt Ltd, Chennai",
+        "specialization":"Full Stack Developer",
+        "MonthOfLeaving":"Jun 2022",
+        "YearOfLeaving":"to Present",
+        "Achievements":"Done live project -> Arul Appalam - Admin Panel (Core PHP, Laravel, HTML & CSS, Jquery)"
+      },
+      {
+        "CompanyName":"Email Album, Chennai",
+        "specialization":"Photoshop Designer",
+        "MonthOfLeaving":"Aug 2020 to",
+        "YearOfLeaving":"Apr 2021",
+        "Achievements":"Designed some Designs and worked in SEO"
+      },
+      {
+        "CompanyName":"Edlar Business Services Pvt Ltd, Chennai.",
+        "specialization":"Full Stack Developer - Intern",
+        "MonthOfLeaving":"Nov 2019 to",
+        "YearOfLeaving":"Apr 2020",
+        "Achievements":"Done a project -> Chatbot & Question Entry (Node JS, MongoDB, HTML & CSS)"
+      }
+    ],
+    "skillsDescription":"My skills are",
+    "skills":[
+      {
+        "skillname":"photoshop"
+      },      
+      {
+        "skillname":"html5"
+      },
+      {
+        "skillname":"css"
+      },
+      {
+        "skillname":"PHP"
+      },
+      {
+        "skillname":"Laravel"
+      },
+      {
+        "skillname":"MySQL"
+      },
+      {
+        "skillname":"jquery"
+      },
+      {
+        "skillname":"ReactJS"
+      },
+      {
+        "skillname":"NodeJS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Arul Appalam - Admin Panel",
+        "description":"Web Application",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"Portfolio",
+        "description":"React JS Website",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"Chatbot & Questions",
+        "description":"Web Application",  
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"Rent O",
+        "description":"Website",
+        "imgurl":"images/portfolio/coffee.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
